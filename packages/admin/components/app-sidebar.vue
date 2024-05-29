2faeb4c2ce45fc67a049b8b664fddf2b8f923d74
daspete <daspetemail@gmail.com>
2024-05-29 07:28:55 +0200

    feat: wip panelmenu in sidebar

--- packages/admin/components/app-sidebar.vue
+++ packages/admin/components/app-sidebar.vue
@@ -1,5 +1,49 @@
 <script setup lang="ts">
+const router = useRouter();
+
 const isOpen = ref(false);
+
+const items = ref([
+    {
+        label: 'Router',
+        icon: 'pi pi-palette',
+        items: [
+            {
+                label: 'Styled',
+                icon: 'pi pi-eraser',
+                route: '/theming'
+            },
+            {
+                label: 'Unstyled',
+                icon: 'pi pi-heart',
+                route: '/unstyled'
+            }
+        ]
+    },
+    {
+        label: 'Programmatic',
+        icon: 'pi pi-link',
+        command: () => {
+            router.push('/introduction');
+        }
+    },
+    {
+        label: 'External',
+        icon: 'pi pi-home',
+        items: [
+            {
+                label: 'Vue.js',
+                icon: 'pi pi-star',
+                url: 'https://vuejs.org/'
+            },
+            {
+                label: 'Vite.js',
+                icon: 'pi pi-bookmark',
+                url: 'https://vuejs.org/'
+            }
+        ]
+    }
+]);
 </script>
 
 <template>
@@ -31,11 +75,40 @@ const isOpen = ref(false);
     </div>
 
     <div>
-      <NuxtLink to="/cms/content-types">
-        <Button text aria-label="Filter" title="Toggle menubar">
-          <i class="i-mdi-file-cog-outline text-primary" />
-        </Button>
-      </NuxtLink>
+      <PanelMenu :model="items" class="w-full" :pt="{ headerContent: 'bg-black' }">
+        <template #item="{ item }">
+          <router-link
+            v-if="item.route"
+            v-slot="{ href, navigate }"
+            :to="item.route"
+            custom
+          >
+            <a
+              v-ripple
+              class="flex items-center cursor-pointer text-primary px-3 py-2"
+              :href="href"
+              @click="navigate"
+            >
+              <span :class="item.icon" />
+              <span class="ml-2 text-color" v-if="isOpen">{{ item.label }}</span>
+            </a>
+          </router-link>
+          <a
+            v-else
+            v-ripple
+            class="flex items-center cursor-pointer text-primary px-3 py-2"
+            :href="item.url"
+            :target="item.target"
+          >
+            <span :class="item.icon" />
+            <span class="ml-2" v-if="isOpen">{{ item.label }}</span>
+            <span
+              v-if="item.items"
+              class="pi pi-angle-down text-primary ml-auto"
+            />
+          </a>
+        </template>
+      </PanelMenu>
     </div>
   </div>
 </template>
