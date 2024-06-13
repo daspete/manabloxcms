06990d9e31b77ccd333599a44042225ccab777e7
daspete <daspetemail@gmail.com>
2024-06-13 17:56:58 +0200

    feat: implemented refresh tokens mechanic

--- packages/admin/plugins/apollo.ts
+++ packages/admin/plugins/apollo.ts
@@ -1,13 +1,18 @@
 import {
   ApolloClient,
   InMemoryCache,
+  Observable,
   createHttpLink,
   from,
+  type FetchResult,
 } from '@apollo/client/core';
 import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
 import { provideApolloClient } from '@vue/apollo-composable';
+import type { AuthTokens } from '~/generated/graphql/graphql';
+import refreshTokensMutation from '~/graphql/auth/refresh-tokens.mutation.gql';
 
-export default defineNuxtPlugin(() => {
+export default defineNuxtPlugin((nuxtApp) => {
   const runtimeConfig = useRuntimeConfig();
   const accessToken = useCookie('accesstoken');
   const refreshToken = useCookie('refreshtoken');
@@ -32,10 +37,69 @@ export default defineNuxtPlugin(() => {
     return { headers: _headers };
   });
 
+  const errorLink = onError((err) => {
+    if (err.graphQLErrors) {
+      for (const graphQLError of err.graphQLErrors) {
+        switch (graphQLError.extensions?.code) {
+          case 'UNAUTHENTICATED':
+          case 'FORBIDDEN':
+            return new Observable<FetchResult>((observer) => {
+              (async () => {
+                if (!refreshToken.value) {
+                  observer.error(err);
+                  return;
+                }
+
+                try {
+                  const { mutate } = await useMutation<{
+                    refreshTokens: AuthTokens;
+                  }>(refreshTokensMutation);
+
+                  const result = await mutate();
+
+                  if (
+                    !result?.data?.refreshTokens?.accessToken ||
+                    !result?.data?.refreshTokens.refreshToken
+                  ) {
+                    observer.error(graphQLError);
+                    return;
+                  }
+
+                  accessToken.value = result.data.refreshTokens.accessToken;
+                  refreshToken.value = result.data.refreshTokens.refreshToken;
+
+                  const subscriber = {
+                    next: observer.next.bind(observer),
+                    error: observer.error.bind(observer),
+                    complete: observer.complete.bind(observer),
+                  };
+
+                  err.operation.setContext({
+                    ...err.operation.getContext(),
+                    headers: {
+                      ...err.operation.getContext().headers,
+                      authorization: `Bearer ${accessToken.value}`,
+                    },
+                  });
+
+                  return err.forward(err.operation).subscribe(subscriber);
+                } catch (err) {
+                  observer.error(err);
+                }
+              })();
+            });
+          break;
+        }
+      }
+    }
+
+    nuxtApp.callHook('apollo:error', err);
+  });
+
   const cache = new InMemoryCache();
 
   const apolloClient = new ApolloClient({
-    link: from([authLink, httpLink]),
+    link: from([errorLink, authLink, httpLink]),
     cache,
   });
 
