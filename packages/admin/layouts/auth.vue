a4276f91191a3c85bcd4dd2eaa90210b1396e908
daspete <daspetemail@gmail.com>
2024-07-01 07:07:16 +0200

    fix: fixed container position on login

--- packages/admin/layouts/auth.vue
+++ packages/admin/layouts/auth.vue
@@ -5,7 +5,7 @@
     <img src="/images/auth.jpg" class="w-full h-full object-cover" />
 
     <div class="w-full h-full absolute top-0 left-0">
-      <div class="container h-full">
+      <div class="container h-full mx-auto">
         <div class="flex justify-end h-full w-full">
           <div
             class="px-4 flex justify-center bg-white/70 backdrop-blur w-[350px] shadow-white/80 shadow-xl"
